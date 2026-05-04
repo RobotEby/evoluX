bbdff4cb1cd44dae9e99b6b0747f66782b467daa
RobotEby <ubuntu.onion@hotmail.com>
2026-05-04 02:23:58 -0300

    feat: adds API function exports to the index module

---
+++ src/shared/api/index.ts
@@ -0,0 +1,3 @@
+export { getApiBaseUrl, isApiConfigured, isSupabaseConfigured } from './env';
+export { apiFetch, type ApiFetchOptions } from './client';
+export { fetchMe, type MeResponse } from './me';
